faab69565afd1b1c57b15695a98cb325d50b2bd7
Christine Elisabeth Koppel <kuutruu@posteo.net>
2024-11-28 10:47:36 +0100

    this does not work!

--- design_sources/conv.vhd
+++ design_sources/conv.vhd
@@ -31,27 +31,50 @@ architecture behaviour of conv is
     -- Derivative in x and y
     signal D_x, D_y,
         sub_result_1, sub_result_2, sub_result_3, sub_result_4, sub_result_5, sub_result_6,
-        sub_result_2_x2, sub_result_5_x2 : signed(n downto 1);
-    signal result : unsigned(n downto 1);
+        sub_result_2_x2, sub_result_5_x2 : signed(10 downto 0);
+    signal result : unsigned(10 downto 0);
+    signal eleven_bit_signed_1, eleven_bit_signed_2, eleven_bit_signed_3, eleven_bit_signed_4,
+      eleven_bit_signed_5, eleven_bit_signed_6, eleven_bit_signed_7, eleven_bit_signed_8 : signed(10 downto 0);
+
 begin
 
     -- Calculate the convolution
     -- D_x(n) = index(1,3)-index_(1,1)+2(index_(2,3)-index_(2,1))+index_(3,3)-index_(3,1)
     -- D_y(n) = index(1,1)-index_(3,1)+2(index_(1,2)-index_(3,2))+index_(1,3)-index_(3,3)
 
+    -- Convert 8-bit unsinged into 11-bit signed by adding MSB as 0 to indicate that it is a positive number
+    eleven_bit_signed_1(10 downto 8) <= "000";
+    eleven_bit_signed_2(10 downto 8) <= "000";
+    eleven_bit_signed_3(10 downto 8) <= "000";
+    eleven_bit_signed_4(10 downto 8) <= "000";
+    eleven_bit_signed_5(10 downto 8) <= "000";
+    eleven_bit_signed_6(10 downto 8) <= "000";
+    eleven_bit_signed_7(10 downto 8) <= "000";
+    eleven_bit_signed_8(10 downto 8) <= "000";
+
+    eleven_bit_signed_1(7 downto 0) <= signed(data_in(1));
+    eleven_bit_signed_2(7 downto 0) <= signed(data_in(2));
+    eleven_bit_signed_3(7 downto 0) <= signed(data_in(3));
+    eleven_bit_signed_4(7 downto 0) <= signed(data_in(4));
+    eleven_bit_signed_5(7 downto 0) <= signed(data_in(5));
+    eleven_bit_signed_6(7 downto 0) <= signed(data_in(6));
+    eleven_bit_signed_7(7 downto 0) <= signed(data_in(7));
+    eleven_bit_signed_8(7 downto 0) <= signed(data_in(8));
+
     -- TODO: Verify that 11 bits (signed) is enough to store the theoretical max value of the calculation
     -- TODO: Optimization: Do bit manipulations instead of 2* (unless vivado already does it)
-    sub_result_1 <= resize(signed(data_in(3)), n) - resize(signed(data_in(1)), n);
-    sub_result_2 <= resize(signed(data_in(5)), n) - resize(signed(data_in(4)), n);
-    sub_result_2_x2 <= sub_result_2(n) & sub_result_2(n-2 downto 1) & '0';
-    -- sub_result_2_x2 <= 2*sub_result_2;
-    sub_result_3 <= resize(signed(data_in(8)), n) - resize(signed(data_in(6)), n);
+    sub_result_1 <= eleven_bit_signed_3 - eleven_bit_signed_1;
+    sub_result_2 <= eleven_bit_signed_5 - eleven_bit_signed_4;
+    sub_result_2_x2 <= eleven_bit_signed_2(n) & sub_result_2(n-2 downto 1) & '0';
+    --sub_result_2_x2 <= 2*sub_result_2;
 
-    sub_result_4 <= resize(signed(data_in(1)), n) - resize(signed(data_in(6)), n);
-    sub_result_5 <= resize(signed(data_in(2)), n) - resize(signed(data_in(7)), n);
+    sub_result_3 <= eleven_bit_signed_8 - eleven_bit_signed_6;
+
+    sub_result_4 <= eleven_bit_signed_1 - eleven_bit_signed_6;
+    sub_result_5 <= eleven_bit_signed_2 - eleven_bit_signed_7;
     sub_result_5_x2 <= sub_result_5(n) & sub_result_5(n-2 downto 1) & '0';
     -- sub_result_5_x2 <= 2*sub_result_5;
-    sub_result_6 <= resize(signed(data_in(3)), n) - resize(signed(data_in(8)), n);
+    sub_result_6 <= eleven_bit_signed_3 - eleven_bit_signed_8;
 
     -- D_x <=  sub_result_1 + 2*sub_result_2 + sub_result_3;
     D_x <=  sub_result_1 + sub_result_2_x2 + sub_result_3;
@@ -59,7 +82,7 @@ begin
 
     -- Calculate the final result
     result <= unsigned(abs(D_x) + abs(D_y));
-    data_out <= resize(result, 8) when (result <= 255) else to_unsigned(255, 8);
+    data_out <= result(7 downto 0) when (result <= 255) else to_unsigned(255, 8);
 
     -- TODO: Make sure the output is cut off at the correct number of bits
     -- TODO: Verify that we are supposed to crop the result at 255 and not... idk. divide it to make 255 equal the max theoretical value of the calculation?
